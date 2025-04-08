d952670ebe46455b4704144bb3019bbec6d3fe11
waliajasmeet <nancywalia206@gmail.com>
2025-04-08 10:55:08 +0530

    Inital commit-1

--- src/main/java/com/mgcfgs/amritsartourism/amritsar_tourism/controller/RegisterController.java
+++ src/main/java/com/mgcfgs/amritsartourism/amritsar_tourism/controller/RegisterController.java
@@ -1,28 +1,39 @@
 package com.mgcfgs.amritsartourism.amritsar_tourism.controller;
 
-// import com.mgcfgs.amritsartourism.amritsar_tourism.model.User;
-// import com.mgcfgs.amritsartourism.amritsar_tourism.repository.UserRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.mgcfgs.amritsartourism.amritsar_tourism.model.User;
+import com.mgcfgs.amritsartourism.amritsar_tourism.repository.UserRepository;
 
 @Controller
 public class RegisterController {
 
-    // @Autowired
-    // private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @GetMapping("/register")
-    public String registerPage() {
-        // model.addAttribute("user", new User());
-        return "user/registerForm";
-    }
+	@GetMapping("/register")
+	public String registerPage(Model model) {
+		model.addAttribute("user", new User());
+		return "user/registerForm";
+	}
 
-    // @PostMapping("/register")
-    // public String registerUser(@ModelAttribute("user") User user, Model model) {
-    //     userRepository.save(user);
-    //     model.addAttribute("message", "User registered successfully!");
-    //     return "user/registerForm";
-    // }
+	@PostMapping("/register")
+	public String registerUser(@ModelAttribute("user") User user, Model model) {
+		userRepository.save(user);
+		model.addAttribute("message", "User registered successfully!");
+		return "redirect:/login";
+	}
+	
+	@GetMapping("/login")
+	public String login(Model model) {
+		return "user/loginForm";
+	}
+	
+	
+	
 }
